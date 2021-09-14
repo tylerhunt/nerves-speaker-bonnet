7269cec6eac5dfdcc99db100bad618a9cc23e44c
Tyler Hunt <tyler@tylerhunt.com>
2021-09-13 22:04:33 -0400

    Generate Nerves app

---
+++ lib/sound_check.ex
@@ -0,0 +1,18 @@
+defmodule SoundCheck do
+  @moduledoc """
+  Documentation for SoundCheck.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SoundCheck.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sound_check_test.exs
@@ -0,0 +1,8 @@
+defmodule SoundCheckTest do
+  use ExUnit.Case
+  doctest SoundCheck
+
+  test "greets the world" do
+    assert SoundCheck.hello() == :world
+  end
+end
